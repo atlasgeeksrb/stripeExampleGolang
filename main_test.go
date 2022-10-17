f13a9b0229968677a34622dbb967d584476d71aa
atlasgeeksrb <rb@atlasgeekservices.com>
2022-10-17 16:32:56 -0400

    Use payment intent id from initiate test, for get test

--- main_test.go
+++ main_test.go
@@ -13,6 +13,35 @@ import (
 	"github.com/stripe/stripe-go/v73"
 )
 
+var paymentIntentId string
+
+func TestInitiatePayment(t *testing.T) {
+	configuration = loadConfiguration()
+
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	initiatePayment(c)
+
+	responseData, readerr := ioutil.ReadAll(w.Body) // []byte
+	if readerr != nil {
+		t.Fatal(readerr)
+	}
+
+	var got stripe.PaymentIntent
+	err := json.Unmarshal(responseData, &got)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ref := reflect.ValueOf(got.ID)
+	if ref.Kind() != reflect.String {
+		assert.Fail(t, "Payment Intent ID not found in response")
+	} else {
+		paymentIntentId = got.ID
+	}
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
 func TestGetPayment(t *testing.T) {
 
 	configuration = loadConfiguration()
@@ -23,7 +52,7 @@ func TestGetPayment(t *testing.T) {
 	// add the payment id param
 	p := gin.Param{
 		Key:   "id",
-		Value: "pi_3LtzZ4B88OE6Qmms0OU5FjJO",
+		Value: paymentIntentId,
 	}
 	c.Params = append(c.Params, p)
 	getPayment(c)
@@ -48,31 +77,5 @@ func TestGetPayment(t *testing.T) {
 	}
 
 	// assert.Equal(t, want, got)
-	assert.Equal(t, http.StatusOK, w.Code)
-}
-
-func TestInitiatePayment(t *testing.T) {
-	configuration = loadConfiguration()
-
-	w := httptest.NewRecorder()
-	c, _ := gin.CreateTestContext(w)
-	initiatePayment(c)
-
-	responseData, readerr := ioutil.ReadAll(w.Body) // []byte
-	if readerr != nil {
-		t.Fatal(readerr)
-	}
-
-	var got stripe.PaymentIntent
-	err := json.Unmarshal(responseData, &got)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	ref := reflect.ValueOf(got.ID)
-	if ref.Kind() != reflect.String {
-		assert.Fail(t, "Payment Intent ID not found in response")
-	}
-
 	assert.Equal(t, http.StatusOK, w.Code)
 }
